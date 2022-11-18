2bccaf2bad021a9d3509c7ff94eee16dff40cba4
BBPCoding <brunopberetta@gmail.com>
2022-11-18 11:15:43 -0300

    First Stage Netflix done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeaturedMoview from "./components/FeaturedMovie";
 import Header from "./components/Header";
 
 // eslint-disable-next-line import/no-anonymous-default-export
+// eslint-disable-next-line
 export default () => {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
@@ -18,12 +19,14 @@ export default () => {
       setMovieList(list);
 
       //Get Featured (Filme em destaque, no fundo, background)
-      let originals = list.filter(i => i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
       let chosen = originals[0].items.results[randomChosen];
 
       //Get Filme or TV Info
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
 
       setFeaturedData(chosenInfo);
     };
@@ -33,28 +36,25 @@ export default () => {
 
   useEffect(() => {
     const scrollListener = () => {
-      if(window.scrollY > 10){
+      if (window.scrollY > 10) {
         setBlackHeader(true);
       } else {
         setBlackHeader(false);
       }
-    }
+    };
 
-    window.addEventListener('scroll', scrollListener);
+    window.addEventListener("scroll", scrollListener);
 
     return () => {
-      window.removeEventListener('scroll', scrollListener);
-    }
-  }, [])
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
 
   return (
     <div className="page">
+      <Header black={blackHeader} />
 
-      <Header black={blackHeader}/>
-
-      {featuredData && 
-        <FeaturedMoview item={featuredData}/>
-      }
+      {featuredData && <FeaturedMoview item={featuredData} />}
 
       <section className="lists">
         {movieList.map((item, key) => (
@@ -62,12 +62,27 @@ export default () => {
         ))}
       </section>
 
-          <footer>
-            Feito com <span role="img" arial-labels="coração">❤️</span> Primeiro Projeto React<br/>
-            Copyright: Netflix<br/>
-            API: themoviedb.org<br/>
-          </footer>
+      <footer>
+        Feito com{" "}
+        <span role="img" arial-labels="coração">
+          ❤️
+        </span>{" "}
+        Primeiro Projeto React
+        <br />
+        Copyright: Netflix
+        <br />
+        API: themoviedb.org
+        <br />
+      </footer>
 
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_1280%2Cc_limit/Netflix_LoadTime.gif"
+            alt="Loading"
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState} from "react";
 import "./MovieRow.css";
+
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ title, items }) => {
+
+  const [scrollX, setScrollX] = useState(0);
+
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2); //metade do tamanho da  tela do usuario
+    if(x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  }
+
+  const handleRigthArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+
+    //largura total da minha lista completa
+    let listW = items.results.length * 150;
+    if((window.innerWidth - listW) > x) {
+      x = (window.innerWidth - listW) - 60;
+    }
+
+    setScrollX(x);
+  }
+
+
   return (
     <div className="movieRow">
       <h2>{title}</h2>
-      <div className="movie-left">
-        
+      
+      <div className="movieRow--left" onClick={handleLeftArrow}>
+        <NavigateBeforeIcon style={{fontSize: 50}}/>
+      </div>
+      <div className="movieRow--right" onClick={handleRigthArrow}>
+        <NavigateNextIcon style={{fontSize: 50}}/>
       </div>
 
       <div className="movieRow--listarea">
-        <div className="moviewRow--list">
+        <div className="moviewRow--list" style={{
+          marginLeft: scrollX, 
+          width: items.results.length * 150}}>
           {items.results.length > 0 &&
             items.results.map((item, key) => (
               <div key={key} className="movieRow--item">
